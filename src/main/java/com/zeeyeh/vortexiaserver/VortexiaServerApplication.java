22e75953779b407781f937d499a91a403452cba2
Qinloren <2336249419@qq.com>
2025-08-23 16:27:45 +0800

    项目初始化完成

---
+++ src/main/java/com/zeeyeh/vortexiaserver/VortexiaServerApplication.java
@@ -0,0 +1,13 @@
+package com.zeeyeh.vortexiaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VortexiaServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VortexiaServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zeeyeh/vortexiaserver/VortexiaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeeyeh.vortexiaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VortexiaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
